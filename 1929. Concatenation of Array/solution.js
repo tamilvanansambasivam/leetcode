e97aa58876ca929230a96668b11570618ac1f91d
tamilvanansambasivam <tamilvanansambasivam@gmail.com>
2022-07-03 20:26:43 +0530

    feat:Concatenation of Array

---
+++ 1929. Concatenation of Array/solution.js
@@ -0,0 +1,12 @@
+var getConcatenation = function(nums) {
+    var ans=[]
+    var i=0;
+        while(i<2){
+            i++;
+            for(j=0;j<nums.length;j++){
+                ans.push(nums[j]);
+            }
+        }
+        return ans;
+    };
+    
