d4fffacd4f123af88c75e61e021aa8b7bb68a1c1
tamilvanansambasivam <tamilvanansambasivam@gmail.com>
2022-07-04 15:50:19 +0530

    1512. Number of Good Pairs

---
+++ 1512. Number of Good Pairs/solution.js
@@ -0,0 +1,9 @@
+var numIdenticalPairs = function(nums) {
+    let ans = 0
+    for(let i=0;i<nums.length-1;i++){
+            for (let j = i+1; j < nums.length; j++) {
+                    if(nums[i]==nums[j]){
+                            ans+=1;
+                    }}}
+  return ans;
+  };
