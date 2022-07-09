7b6f20b8533933825ea5f6039c6f206b58f37f3b
tamilvanansambasivam <tamilvanansambasivam@gmail.com>
2022-07-09 19:43:44 +0530

    125. Valid Palindrome

---
+++ 125. Valid Palindrome/solution.js
@@ -0,0 +1,13 @@
+var isPalindrome = function(s) {
+    s = s.replace(/[^a-z0-9]/gi,"").toLowerCase();
+    
+    r=s.split("").reverse().join("")
+     
+    if(s==r || s=="" || r==""){
+        return true
+    }
+    
+    else{
+        return false
+    }
+    };
