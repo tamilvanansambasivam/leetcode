60157766ee8a7fefc5692125a35dd9a9fd2f5231
tamilvanansambasivam <tamilvanansambasivam@gmail.com>
2022-07-17 16:40:21 +0530

    format code

--- 1512. Number of Good Pairs/solution.js
+++ 1512. Number of Good Pairs/solution.js
@@ -1,9 +1,11 @@
-var numIdenticalPairs = function(nums) {
-    let ans = 0
-    for(let i=0;i<nums.length-1;i++){
-            for (let j = i+1; j < nums.length; j++) {
-                    if(nums[i]==nums[j]){
-                            ans+=1;
-                    }}}
-  return ans;
-  };
+var numIdenticalPairs = function (nums) {
+        let ans = 0
+        for (let i = 0; i < nums.length - 1; i++) {
+                for (let j = i + 1; j < nums.length; j++) {
+                        if (nums[i] == nums[j]) {
+                                ans += 1;
+                        }
+                }
+        }
+        return ans;
+};
