a441b12ed09c38e5a94c39de96078e139ea0446b
tamilvanansambasivam <tamilvanansambasivam@gmail.com>
2022-07-17 16:35:48 +0530

    format code

--- 1672. Richest Customer Wealth/solution.js
+++ 1672. Richest Customer Wealth/solution.js
@@ -1,12 +1,11 @@
-var maximumWealth = function(accounts) {
-    let maxWealth=0;
-    for(let i=0;i<accounts.length;i++){
-        let sum=accounts[i].reduce((a,b)=> a + b);
-        if (sum > maxWealth){
+var maximumWealth = function (accounts) {
+    let maxWealth = 0;
+    for (let i = 0; i < accounts.length; i++) {
+        let sum = accounts[i].reduce((a, b) => a + b);
+        if (sum > maxWealth) {
             maxWealth = sum;
         }
     }
     return maxWealth;
-    };
+};
 
-    
