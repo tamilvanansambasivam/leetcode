c927cc3faa91280969e39ff68523a93db3034b6d
tamilvanansambasivam <tamilvanansambasivam@gmail.com>
2022-07-17 16:58:01 +0530

    fix: formatted the documented

--- 9. Palindrome Number/solution.js
+++ 9. Palindrome Number/solution.js
@@ -1,4 +1,4 @@
-var isPalindrome = function(x) {
+var isPalindrome = function (x) {
     let y = parseInt(x.toString().split('').reverse().join(''));
-        return x===y;
-    };
+    return x === y;
+};
