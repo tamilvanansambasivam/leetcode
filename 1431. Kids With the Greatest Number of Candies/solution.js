866f3498ea9f2abe8c138a196941f651d6befcf1
tamilvanansambasivam <tamilvanansambasivam@gmail.com>
2022-07-17 16:43:19 +0530

    format code

--- 1431. Kids With the Greatest Number of Candies/solution.js
+++ 1431. Kids With the Greatest Number of Candies/solution.js
@@ -1,11 +1,12 @@
-var kidsWithCandies = function(candies, extraCandies) {
-    let ans=[];
-    let maxValue= Math.max(...candies)
+var kidsWithCandies = function (candies, extraCandies) {
+    let ans = [];
+    let maxValue = Math.max(...candies)
     for (let i = 0; i < candies.length; i++) {
-        if(candies[i]+extraCandies>=maxValue){
+        if (candies[i] + extraCandies >= maxValue) {
             ans.push(true);
-        } else{
+        } else {
             ans.push(false);
-        }}
-        return ans;
-    };
+        }
+    }
+    return ans;
+};
