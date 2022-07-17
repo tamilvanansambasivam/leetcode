b3cf8c40b9029fabc7d8d9bce2a9b056a1c3a3e8
tamilvanansambasivam <tamilvanansambasivam@gmail.com>
2022-07-17 16:38:43 +0530

    format code

--- 1528. Shuffle String/solution.js
+++ 1528. Shuffle String/solution.js
@@ -1,8 +1,8 @@
-var restoreString = function(s, indices) {
+var restoreString = function (s, indices) {
     const ans = new Array(indices.length)
-    for (i=0;i<indices.length;i++){
-        ans[indices[i]]= s[i];
-        
+    for (i = 0; i < indices.length; i++) {
+        ans[indices[i]] = s[i];
+
     }
     return ans.join('')
-    };
+};
