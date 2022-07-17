a5293fe6c9dfc3e83cfe6e39f322ccfe023d570f
tamilvanansambasivam <tamilvanansambasivam@gmail.com>
2022-07-17 16:52:41 +0530

    fix: formatted the documented

--- 125. Valid Palindrome/solution.js
+++ 125. Valid Palindrome/solution.js
@@ -2,7 +2,7 @@ var isPalindrome = function (s) {
   s = s.replace(/[^a-z0-9]/gi, "").toLowerCase();
 
   r = s.split("").reverse().join("");
-
+  
   if (s == r || s == "" || r == "") {
     return true;
   } else {
