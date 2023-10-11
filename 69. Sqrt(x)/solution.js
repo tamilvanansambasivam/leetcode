e4a17fc73a830b770129dd0692ff48e1b529b40b
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2023-10-11 23:23:01 +0530

    Update solution.js

--- 69. Sqrt(x)/solution.js
+++ 69. Sqrt(x)/solution.js
@@ -1,9 +1,14 @@
-//To  find the square root of number
-
 mySqrt = function (x) {
-    let ans = 0;
+    let ans = 0;  // Start with a variable called 'ans' set to 0.
+
+    // Start a loop with a counter 'i' set to 0. Keep looping as long as 'i * i' is less than or equal to 'x'.
     for (i = 0; i * i <= x; i++) {
-        ans = i;
+        ans = i;  // If 'i * i' is less than or equal to 'x', update the 'ans' variable with the current value of 'i'.
     }
-    return ans;
+
+    return ans;  // Finally, return the value stored in 'ans' as the result.
 }
+
+
+
+
