cfc10eab11c7350b9fa959106f00b90752a10536
tamilvanan <61337194+tamilvanansambasivam@users.noreply.github.com>
2023-12-10 16:58:50 +0530

    Update solution.js

--- 1. Two Sum/solution.js
+++ 1. Two Sum/solution.js
@@ -1,21 +1,30 @@
 /**
- * @param {number[]} nums
- * @param {number} target
- * @return {number[]}
+ * Finds and returns the indices of two numbers in an array whose sum is equal to the target.
+ *
+ * @param {number[]} nums - The array of numbers.
+ * @param {number} target - The target sum.
+ * @returns {number[]} - An array containing the indices of the two numbers.
  */
-
 var twoSum = function(nums, target) {
+    // Create a hash map to store the difference between the target and each number along with its index.
     const mp = {};
 
-   for (let i = 0; i < nums.length; i++) {
-   	const diff = target - nums[i];
-   
-   	if (diff in mp) return [mp[diff],i];
+    // Iterate through the array of numbers.
+    for (let i = 0; i < nums.length; i++) {
+        // Calculate the difference between the target and the current number.
+        const diff = target - nums[i];
 
-   	mp[nums[i]] = i;
-   }
+        // Check if the difference is already in the hash map.
+        // If yes, return the indices of the two numbers that add up to the target.
+        if (diff in mp) {
+            return [mp[diff], i];
+        }
 
+        // If the difference is not in the hash map, store the current number and its index in the map.
+        mp[nums[i]] = i;
+    }
 };
+
 
 // time complexity  : O(n)
 // space complexity : O(n)
