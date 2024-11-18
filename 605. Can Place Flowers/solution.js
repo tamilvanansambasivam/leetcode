261e67eb233cd2b421fff806a916c4639bc3084e
tamilvanan <tamilvanansambasivam@gmail.com>
2024-11-18 10:40:44 +0530

    605. Can Place Flowers

--- 605. Can Place Flowers/solution.js
+++ 605. Can Place Flowers/solution.js
@@ -7,33 +7,12 @@ var canPlaceFlowers = function (flowerbed, n) {
   if (n == 0) {
     return true;
   }
-  if (flowerbed.length == 0 || flowerbed.length == 1) {
-    if (flowerbed[flowerbed.length - 1] == 0) {
-      return n <= 1;
-    }
-  }
 
   for (let i = 0; i < flowerbed.length; i++) {
-    if (i == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
-      flowerbed[i] = 1;
-      n--;
-      if (n == 0) {
-        return true;
-      }
-    } else if (
-      i == flowerbed.length - 1 &&
+    if (
       flowerbed[i] == 0 &&
-      flowerbed[i - 1] == 0
-    ) {
-      flowerbed[i] = 1;
-      n--;
-      if (n == 0) {
-        return true;
-      }
-    } else if (
-      flowerbed[i] == 0 &&
-      flowerbed[i - 1] == 0 &&
-      flowerbed[i + 1] == 0
+      (i == 0 || flowerbed[i - 1] == 0) &&
+      (i == flowerbed.length - 1 || flowerbed[i + 1] == 0)
     ) {
       flowerbed[i] = 1;
       n--;
@@ -43,61 +22,5 @@ var canPlaceFlowers = function (flowerbed, n) {
     }
   }
 
-  if (n == 0) {
-    return true;
-  } else {
-    return false;
-  }
-};
-
-/**
- * @param {number[]} flowerbed
- * @param {number} n
- * @return {boolean}
- */
-var canPlaceFlowers = function (flowerbed, n) {
-  if (n == 0) {
-    return true;
-  }
-  if (flowerbed.length == 0 || flowerbed.length == 1) {
-    if (flowerbed[flowerbed.length - 1] == 0) {
-      return n <= 1;
-    }
-  }
-
-  for (let i = 0; i < flowerbed.length; i++) {
-    if (i == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
-      flowerbed[i] = 1;
-      n--;
-      if (n == 0) {
-        return true;
-      }
-    } else if (
-      i == flowerbed.length - 1 &&
-      flowerbed[i] == 0 &&
-      flowerbed[i - 1] == 0
-    ) {
-      flowerbed[i] = 1;
-      n--;
-      if (n == 0) {
-        return true;
-      }
-    } else if (
-      flowerbed[i] == 0 &&
-      flowerbed[i - 1] == 0 &&
-      flowerbed[i + 1] == 0
-    ) {
-      flowerbed[i] = 1;
-      n--;
-      if (n == 0) {
-        return true;
-      }
-    }
-  }
-
-  if (n == 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return n === 0;
 };
